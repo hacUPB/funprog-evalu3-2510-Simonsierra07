5ca802df8f6e44767239cda0de97200f74aa3569
Simonsierra07 <simon.sierrav@upb.edu.co>
2025-03-13 14:16:21 +0000

    calculador 😊

--- calculadora.c
+++ calculadora.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
+int suma(int num1, int num2) {
+    return num1 + num2;
+}
+
+int resta(int num1, int num2) {
+    return num1 - num2;
+}
+
+int multiplicacion(int num1, int num2) {
+    return num1 * num2;
+}
+
+float division(int num1, int num2) {
+    if (num2 == 0) {
+        printf("Error: División por cero.\n");
+        return 0;  // Retornar 0 en caso de error
+    }
+    return (float)num1 / num2;
+}
+
 int main() {
     int num1, num2;
     int opcion;
 
     printf("Ingrese el primer número: ");
-    scanf("%f", &num1);
+    scanf("%d", &num1);  
     printf("Ingrese el segundo número: ");
-    scanf("%f", &num2);
+    scanf("%d", &num2);  
 
     printf("Seleccione la operación a realizar:\n");
     printf("1. Suma\n2. Resta\n3. Multiplicación\n4. División\n");
@@ -17,16 +37,16 @@ int main() {
 
     switch(opcion) {
         case 1:
-            suma(num1, num2);
+            printf("Resultado: %d\n", suma(num1, num2));
             break;
         case 2:
-            resta(num1, num2);
+            printf("Resultado: %d\n", resta(num1, num2));
             break;
         case 3:
-            multiplicacion(num1, num2);
+            printf("Resultado: %d\n", multiplicacion(num1, num2));
             break;
         case 4:
-            division(num1, num2);
+            printf("Resultado: %.2f\n", division(num1, num2));
             break;
         default:
             printf("Opción no válida.\n");
