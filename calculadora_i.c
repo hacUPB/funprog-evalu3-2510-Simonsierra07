db0016a33ad42fe8a06bb3fb5834700948cf8954
Simonsierra07 <simon.sierrav@upb.edu.co>
2025-04-02 18:09:40 -0500

    Update calculadora_i.c

--- calculadora_i.c
+++ calculadora_i.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
 
+void calcularLeyOhm();
+void calcularFactorDePotencia();
+void calcularResistenciaConductor();
+void calcularResistenciaLED();
+
+int main() {
+    int opcion;
+
+    do {
+        printf("\nBienvenido a la Calculadora de Ingeniería\n");
+        printf("Por favor, selecciona una opción:\n");
+        printf("1. Calcular Ley de Ohm\n");
+        printf("2. Calcular Factor de Potencia\n");
+        printf("3. Calcular Resistencia de un Conductor\n");
+        printf("4. Calcular Valor de la Resistencia para un LED\n");
+        printf("5. Salir\n");
+        printf("Ingresa el número de la opción deseada: ");
+        scanf("%d", &opcion);
+
+        switch(opcion) {
+            case 1:
+                calcularLeyOhm();
+                break;
+            case 2:
+                calcularFactorDePotencia();
+                break;
+            case 3:
+                calcularResistenciaConductor();
+                break;
+            case 4:
+                calcularResistenciaLED();
+                break;
+            case 5:
+                printf("¡Gracias por usar la calculadora!\n");
+                break;
+            default:
+                printf("Opción no válida, intenta de nuevo.\n");
+        }
+    } while(opcion != 5);
+
+    return 0;
+}
+esistenciaConductor();
+                break;
+            case 4:
+                calcularResistenciaLED();
+                break;
+            case 5:
+                printf("¡Gracias por usar la calculadora!\n");
+                break;
+            default:
+                printf("Opción no válida, intenta de nuevo.\n");
+        }
+    } while(opcion != 5);
+
+
+    return 0;
+}
